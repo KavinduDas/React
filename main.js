6f7d00572f7e46ddcf241e2b269cc00a5769876a
Kavindu Dassanayake <kavindud@AFSL.local>
2025-02-21 21:35:02 +0530

    My first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// document.addEventListener("DOMContentLoaded",() =>{
+//     console.log("document is ready")
+// })
+
+var message;
+message = "Hello , World !";
+
+
+function myFunction(){
+   var myvariavle = "kavindu"
+   
+   return myvariavle
+   
+   
+}
+console.log("LOG:",myFunction());
+
